3ddca384de76c36180aa4f28cbc75a3a857faaff
Saar1122 <61451069+Saar1122@users.noreply.github.com>
2020-03-26 15:10:34 +0200

    Update docker-auto.py

--- docker-auto.py
+++ docker-auto.py
@@ -12,28 +12,33 @@ def build():
    '''
    build image
    '''
-   make = "docker build . -t saar"
+   make = "docker build . -t '" + name + "' "
    subprocess.run(make, shell=True)
 
-def run():
+def run(name, project):
    '''
    run docker
    '''
-   run_docker = "docker run -p 3000:3000 -d saar"
+   run_docker = "docker run -p 3000:3000 -d '" + name + "'"
    subprocess.run(run_docker, shell=True)
 
 def get_ip_curl():
     '''
     catch local ip and do curl
     '''
-    time.sleep(4)
-    get_ip_cmd = "docker ps | grep saar | awk '{print $10}' | awk -F: '{print $1}'"
+    time.sleep(3)
+    get_ip_cmd = "docker ps | grep '" + name + "' | awk '{print $10}' | awk -F: '{print $1}'"
+    id = "docker ps -a | grep seco | awk '{print $1}'"
     ip = subprocess.run(get_ip_cmd, shell=True, capture_output=True)
+    get_id = subprocess.run(id, shell=True, capture_output=True)
     new_ip = ip.stdout.decode().strip().lower()
+    new_id = get_id.stdout.decode().strip().lower()
     port = ":3000"
     #curl to ip
     url = requests.post("http://" + new_ip + port)
+    #print("container id", get_id)
     print("\nThe answer is:", url.text)
+    print("The container id is:", new_id)
     if url.text:
         print("the Container is ok!\n")
     else:
