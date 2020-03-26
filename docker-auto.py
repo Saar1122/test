675d0ce258cbbf1ede414f85c2aa1494041699f5
Saar1122 <61451069+Saar1122@users.noreply.github.com>
2020-03-26 16:19:54 +0200

    Update docker-auto.py

--- docker-auto.py
+++ docker-auto.py
@@ -12,14 +12,14 @@ def build():
    '''
    build image
    '''
-   make = "docker build . -t '" + name + "' "
+   make = "docker build . -t saar"
    subprocess.run(make, shell=True)
 
 def run(name, project):
    '''
    run docker
    '''
-   run_docker = "docker run -p 3000:3000 -d '" + name + "'"
+   run_docker = "docker run -p 3000:3000 -d saar"
    subprocess.run(run_docker, shell=True)
 
 def get_ip_curl():
@@ -27,7 +27,7 @@ def get_ip_curl():
     catch local ip and do curl
     '''
     time.sleep(3)
-    get_ip_cmd = "docker ps | grep '" + name + "' | awk '{print $10}' | awk -F: '{print $1}'"
+    get_ip_cmd = "docker ps | grep saar | awk '{print $10}' | awk -F: '{print $1}'"
     id = "docker ps -a | grep seco | awk '{print $1}'"
     ip = subprocess.run(get_ip_cmd, shell=True, capture_output=True)
     get_id = subprocess.run(id, shell=True, capture_output=True)
